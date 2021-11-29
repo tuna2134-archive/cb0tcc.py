1b0b6f98d8b333b74c6cbfa33ced5a9ac4024b1e
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-29 15:50:45 +0900

    Add:view

--- cb0tcc/main.py
+++ cb0tcc/main.py
@@ -1,20 +1,31 @@
 import requests
 from .error import *
+import re
 
 class cb0t:
-    def __init__(self, url:str):
+    def create(self, url:str):
         """
         This can create shorten url.
         """
-        self.r = requests.post("https://cb0t.cc/api/create", data = {"url": url})
-        if self.r.status_code == 400:
+        r = requests.post("https://cb0t.cc/api/create", data = {"url": url})
+        if r.status_code == 400:
             raise Invalidurl("Invalid url specified.")
-        elif self.r.status_code == 406:
+        elif r.status_code == 406:
             raise Invalidurl("Sorry but the specified domain is on our blacklist.")
-
-    @property
-    def url(self):
-        """
-        This return shorten url.
-        """
-        return self.r.json()["url"]
+        else:
+            return r.json()["url"]
+            
+    def view(self, url:str):
+        url = re.compile(r"https?://cb0t.cc/(\d+)")
+        if re.match(url) is not None:
+            code = re.match(url)[0]
+            query = {
+                "code": code
+            }
+            r = request.get("https://cb0t.cc/api/view", params = query)
+            if r.status_code == 404:
+                raise Invalidurl("I can't found that url")
+            else:
+                return r.json()["url"]
+        else:
+            raise Invaildurl("This url is not vaild")
